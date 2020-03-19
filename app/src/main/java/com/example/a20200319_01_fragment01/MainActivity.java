fdacb967f18b6becdc13c4a578fc8f8981080833
hjin302 <enro302@naver.com>
2020-03-19 09:58:02 +0900

    첫 프래그먼트

---
+++ app/src/main/java/com/example/a20200319_01_fragment01/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.a20200319_01_fragment01;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.example.a20200319_01_fragment01.databinding.ActivityMainBinding;
+
+import com.example.a20200319_01_fragment01.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding binding = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
