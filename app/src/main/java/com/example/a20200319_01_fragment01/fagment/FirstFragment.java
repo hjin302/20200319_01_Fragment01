1dae6b61f12908fbaa828b818cc6b66cfedae859
hjin302 <enro302@naver.com>
2020-03-19 10:56:09 +0900

    프래그먼트 데이터바인딩 연동

--- app/src/main/java/com/example/a20200319_01_fragment01/fagment/FirstFragment.java
+++ app/src/main/java/com/example/a20200319_01_fragment01/fagment/FirstFragment.java
@@ -6,16 +6,22 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.a20200319_01_fragment01.R;
+import com.example.a20200319_01_fragment01.databinding.FragmentFirstBinding;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 public class FirstFragment extends Fragment {
+
+    FragmentFirstBinding binding = null;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_first, container, false);
+        return binding.getRoot();
     }
 
     @Override
